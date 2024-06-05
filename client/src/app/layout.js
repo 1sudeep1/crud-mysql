93930ed55c9ea43dc82374f1be6231b26be4c217
Sudeep Tharu <sudeep.tharu32@gmail.com>
2024-06-05 08:47:33 +0545

    navbar and home page hero section done

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer, toast } from 'react-toastify';
+import Navbar from "./components/Navbar/page";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <ToastContainer />
         {children}
       </body>
